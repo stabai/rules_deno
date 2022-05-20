854ce9ddd5ab619d27049c868adae45283f2492f
Shawn Tabai <shawn.tabai@actualhq.com>
2022-05-20 12:14:24 -0700

    test

--- e2e/workspace/deno_binary/example_test.sh
+++ e2e/workspace/deno_binary/example_test.sh
@@ -1,23 +1,20 @@
 #!/usr/bin/env bash
 set -e
 
-# echo "$SCRIPT_LOCATION"
-# cat "$SCRIPT_LOCATION"
 export DENO_DIR="$PWD/.deno"
 mkdir -p "$DENO_DIR/cache/gen"
-echo "$DENO_DIR"
 
 $SCRIPT_LOCATION script_output.json
 
-# echo "Deno execution finished"
+echo "Deno execution finished"
 
-# expected_output='{"run":"prompt","read":"prompt","write":"granted","net":"prompt","env":"granted","ffi":"prompt","hrtime":"prompt"}'
-# actual_output="$(cat script_output.json)"
+expected_output='{"run":"prompt","read":"prompt","write":"granted","net":"prompt","env":"granted","ffi":"prompt","hrtime":"prompt"}'
+actual_output="$(cat script_output.json)"
 
-# if [[ "$expected_output" != "$actual_output" ]]; then
-#   echo "------------------------------------------"
-#   echo "ERROR: Unexpected output from Deno script."
-#   printf "Expected:\n%s\n" "$expected_output" >&2
-#   printf "Actual:\n%s\n" "$actual_output" >&2
-#   exit 1
-# fi
+if [[ "$expected_output" != "$actual_output" ]]; then
+  echo "------------------------------------------"
+  echo "ERROR: Unexpected output from Deno script."
+  printf "Expected:\n%s\n" "$expected_output" >&2
+  printf "Actual:\n%s\n" "$actual_output" >&2
+  exit 1
+fi
